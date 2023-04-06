e8e8c65581cc14068d92712213f85bcec0e9f4eb
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-04-06 15:26:54 +0200

    Refactoring code: create functions (DRY principle)

--- script.js
+++ script.js
@@ -6,21 +6,38 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20; // creo variabile score che parte da 20 (punteggio MAX)
 let highscore = 0; // creo variabile highscore che parte da 0
 
+// creo funzione per non duplicare codice della visualizzazione messaggio
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+// creo funzione per non duplicare codice della visualizzazione score
+const displayScore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+
+// creo funzione per non duplicare codice del colore di background del body
+const displayBgBody = function (color) {
+  document.querySelector('body').style.backgroundColor = color;
+};
+
+// creo variabile per non duplicare codice per la selezione della classe .number
+const displayNumber = document.querySelector('.number');
+
 document.querySelector('.check').addEventListener('click', function () {
-  // console.log(document.querySelector('.guess').value);
   const guess = Number(document.querySelector('.guess').value); // salvo il valore in una variabile chiamata guess
   console.log(typeof guess, guess); // i valori ottenuti dall'UI, in questo caso da un campo input, sono solitamente stringhe per cui dobbiamo trasformarli in numeri
 
   // Quando non c'è l'input (non viene inserito alcun numero oppure viene inserito 0 che corrisponde a falso)
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ Nessun numero!';
+    displayMessage('⛔ Nessun numero!');
 
     // Quando il giocatore indovina il numero segreto
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Numero corretto!';
-    document.querySelector('body').style.backgroundColor = '#60b347'; // cambio il colore di sfondo
-    document.querySelector('.number').textContent = secretNumber; // svelo il numero nel container solamente quando il giocatore indovina
-    document.querySelector('.number').style.width = '30rem'; // cambio la larghezza del container del numero segreto
+    displayMessage('🎉 Numero corretto!');
+    displayBgBody('#60b347'); // cambio il colore di sfondo
+    displayNumber.textContent = secretNumber; // svelo il numero nel container solamente quando il giocatore indovina
+    displayNumber.style.width = '30rem'; // cambio la larghezza del container del numero segreto
 
     // controllo highscore
     if (score > highscore) {
@@ -30,43 +47,34 @@ document.querySelector('.check').addEventListener('click', function () {
 
     // Quando il numero è superiore a 20 o minore di 0
   } else if (guess > 20 || guess < 0) {
-    document.querySelector('.message').textContent =
-      '🚫 Non è un numero compreso tra 1 e 20!';
+    displayMessage('🚫 Non è un numero compreso tra 1 e 20!');
 
-    // Quando il numero è troppo alto
-  } else if (guess > secretNumber) {
+    // Quando il numero NON è indovinato
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Numero troppo alto!';
+      displayMessage(
+        guess > secretNumber
+          ? '📈 Numero troppo alto!'
+          : '📉 Numero troppo basso!'
+      );
       score--; // OPPURE score -= 1; OPPURE score = score - 1;
-      document.querySelector('.score').textContent = score;
+      displayScore(score);
     } else {
-      document.querySelector('.message').textContent = '❌ Hai perso!';
-      document.querySelector('.score').textContent = 0;
-    }
-
-    // Quando il numero è troppo basso
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent =
-        '📉 Numero troppo basso!';
-      score--; // OPPURE score -= 1; OPPURE score = score - 1;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '❌ Hai perso!';
-      document.querySelector('.score').textContent = 0;
+      displayMessage('❌ Hai perso!');
+      displayScore(0);
     }
   }
 });
 
-// Implemento funzionalità per ripristinare il gioco da zero cliccando sul tasto "Riprova!"
+// Implemento funzionalità per resettare il gioco cliccando sul tasto "Riprova!"
 document.querySelector('.again').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1; // genero nuovo numero segreto
   score = 20; // ripristino punteggio iniziale
 
-  document.querySelector('.score').textContent = score; // punteggio
-  document.querySelector('.message').textContent = 'Inizia a indovinare...'; // messaggio
+  displayScore(score); // punteggio
+  displayMessage('Inizia a indovinare...'); // messaggio
   document.querySelector('.guess').value = ''; // valore input
-  document.querySelector('body').style.backgroundColor = '#222'; // colore sfondo
-  document.querySelector('.number').textContent = '?'; // container numero segreto
-  document.querySelector('.number').style.width = '15rem'; // larghezza container numero segreto
+  displayBgBody('#222'); // colore sfondo
+  displayNumber.textContent = '?'; // container numero segreto
+  displayNumber.style.width = '15rem'; // larghezza container numero segreto
 });
